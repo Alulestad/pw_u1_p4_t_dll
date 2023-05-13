aca8e4c71ea6867673afb99e17678b6164688790
Alulestad <vladimirlestad@gmail.com>
2023-05-13 11:51:51 -0500

    Primer commit del repositorio

---
+++ src/main/java/com/example/demo/PwU1P4TDllApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P4TDllApplication implements CommandLineRunner{
+	
+	public static final Logger LOG=LoggerFactory.getLogger(PwU1P4TDllApplication.class);
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("Segundo programa en STS");
+		
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P4TDllApplication.class, args);
+	}
+	
+
+}
